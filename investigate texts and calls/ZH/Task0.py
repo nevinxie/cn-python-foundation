04e86c94a6c9beeee596673293253c002ace510c
Nevin Xie <nevin.xie@qq.com>
2017-12-23 21:28:16 +0800

    Task0
    Finished Task0

--- investigate texts and calls/ZH/Task0.py
+++ investigate texts and calls/ZH/Task0.py
@@ -19,4 +19,9 @@ with open('calls.csv', 'r') as f:
 "First record of texts, <incoming number> texts <answering number> at time <time>"
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
+firstText = texts[0]
+lastCall = calls[-1]
+
+print("First record of texts, {0} texts {1} at time {2}".format(firstText[0], firstText[1], firstText[2]))
+print("Last record of calls, {0} calls {1} at time {2}, lasting {3} seconds".format(lastCall[0], lastCall[1], lastCall[2], lastCall[3]))
 
