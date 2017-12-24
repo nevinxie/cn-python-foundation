5299d2df5288b73db78c18658e2ed00ff446dc95
Nevin Xie <nevin.xie@qq.com>
2017-12-24 22:08:06 +0800

    Task2
    Finished task2 for counting numbers in texts and calls

--- investigate texts and calls/ZH/Task1.py
+++ investigate texts and calls/ZH/Task1.py
@@ -16,4 +16,15 @@ with open('calls.csv', 'r') as f:
 任务1：
 短信和通话记录中一共有多少电话号码？每个号码只统计一次。
 输出信息：
-"There are <count> different telephone numbers in the records.""""
+"There are <count> different telephone numbers in the records."""
+numbers = set([])
+
+for text in texts:
+	numbers.add(text[0])
+	numbers.add(text[1])
+
+for call in calls:
+	numbers.add(call[0])
+	numbers.add(call[1])
+
+print("There are {0} different telephone numbers in the records.".format(len(numbers)))		
