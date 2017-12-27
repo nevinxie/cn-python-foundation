39f0ed668b915d9547290f07bc378a1b3d4173e7
Nevin Xie <nevin.xie@qq.com>
2017-12-27 22:37:25 +0800

    Task3
    Create Movie class

--- best movie/DoubanCrawler.py
+++ best movie/DoubanCrawler.py
@@ -1,5 +1,28 @@
 import expanddouban
 
+
+class Movie:
+	pass
+	def __init__(self, name, rate, location, category, info_link, cover_link):
+		self.name = name
+		self.rate = rate
+		self.location = location
+		self.category = category
+		self.info_link = info_link
+		self.cover_link = cover_link	
+
+
+name = "肖申克的救赎"
+rate = 9.6
+location = "美国"
+category = "剧情"
+info_link = "https://movie.douban.com/subject/1292052/"
+cover_link = "https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p480747492.jpg"
+
+m = Movie(name, rate, location, category, info_link, cover_link)
+
+#print(m.cover_link)
+
 """
 return a string corresponding to the URL of douban movie lists given category and location.
 """
@@ -10,6 +33,6 @@ def getMovieUrl(category, location):
 
 #print(getMovieUrl("剧情","大陆"))	
 url = getMovieUrl("剧情","大陆")
-html = expanddouban.getHtml(url)
+#html = expanddouban.getHtml(url)
 
-print(html)
+#print(html)
